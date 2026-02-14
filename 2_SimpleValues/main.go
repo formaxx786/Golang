2f69f0f8a2acd79de984c0d3631b979b25ef8c60
formaxx786 <formaxx2000@gmail.com>
2026-02-14 18:31:19 +0530

    first commit

---
+++ 2_SimpleValues/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	//integers
+	fmt.Println(42)
+	fmt.Println(2 + 3)
+	//strings
+	fmt.Println("Hello, Go!")
+	//booleans
+	fmt.Println(false)
+	fmt.Println(true)
+	//floats
+	fmt.Println(3.14)
+	fmt.Println(7.0 / 3.0)
+}
