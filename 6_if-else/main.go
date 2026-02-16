9c427a0ef8eb5cd601798c77b734f590391b4be3
formaxx786 <formaxx2000@gmail.com>
2026-02-16 12:04:15 +0530

    Add switch statement examples demonstrating simple and multiple conditions

---
+++ 6_if-else/main.go
@@ -0,0 +1,30 @@
+package main
+
+func main() {
+	// age := 16
+	// if age >= 18 {
+	// 	fmt.Println("You are an adult.")
+	// } else {
+	// 	fmt.Println("You are a minor.")
+	// }
+	// if age >= 18 {
+	// 	fmt.Println("You are an adult.")
+	// } else if age >= 12 {
+	// 	fmt.Println("You are a teenager.")
+	// } else {
+	// 	fmt.Println("You are a child.")
+	// }
+
+	// var role = "admin"
+	// var haspermission = true
+	// if role == "admin" || haspermission {
+	// 	fmt.Println("Access granted.")
+	// }
+	// if age := 15; age >= 18 {
+	// 	fmt.Println("You are an adult.")
+	// } else if age >= 13 {
+	// 	fmt.Println("You are a minor.")
+	// }
+
+	// go does not have ternary operator, you will have to use if-else
+}
---
+++ 7_switch/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// simple switch statement
+	// i := 5
+	// switch i {
+	// case 1:
+	// 	fmt.Println("one")
+	// case 2:
+	// 	fmt.Println("two")
+	// case 3:
+	// 	fmt.Println("three")
+	// default:
+	// 	fmt.Println("other")
+	// }
+
+	//multiple conditions switch statement
+
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend!")
+	default:
+		fmt.Println("It's a weekday.")
+	}
+
+}
