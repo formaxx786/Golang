19e0bfb2743a33f9a5c9dc488e1befdcb1916148
formaxx786 <formaxx2000@gmail.com>
2026-02-16 12:32:19 +0530

    Remove unused time package and comment out multiple conditions switch statement

--- 7_switch/main.go
+++ 7_switch/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
@@ -21,11 +20,29 @@ func main() {
 
 	//multiple conditions switch statement
 
-	switch time.Now().Weekday() {
-	case time.Saturday, time.Sunday:
-		fmt.Println("It's the weekend!")
-	default:
-		fmt.Println("It's a weekday.")
+	// switch time.Now().Weekday() {
+	// case time.Saturday, time.Sunday:
+	// 	fmt.Println("It's the weekend!")
+	// default:
+	// 	fmt.Println("It's a weekday.")
+	// }
+	// type switch statement
+
+	whoAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case int:
+			fmt.Println("I'm an integer with value \n", t)
+
+		case string:
+			fmt.Println("I'm a string with value \n", t)
+
+		case bool:
+			fmt.Println("I'm a boolean with value \n", t)
+		default:
+			fmt.Printf("I don't know what type I am\n", t)
+
+		}
 	}
+	whoAmI(43)
 
 }
